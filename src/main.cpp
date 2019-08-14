54e35346d2b5bc6a661fd23a415b2792d156198c
Оганесян Левон Артурович <ilev4ik@gmail.com>
2019-08-14 14:59:26 +0300

    maximizer function refactored ready to test

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <thread>
-#include <future>
 #include <random>
 
 #include "banner_traits.h"
@@ -13,17 +11,18 @@
 #include "hash_back_inserter.h"
 #include "simple_thread_pool.h"
 
-struct banner
+class banner
 {
     using rank_t = int;
+    rank_t rank() const { return price; }
+public:
+    friend class banner_traits<banner>;
     banner(int id, int pr) : adv_id(id), price(pr) {}
 	banner(int id, int pr, const std::string& country)
 	    : adv_id(id), price(pr)
 	{
 		countries.insert(country);
 	}
-
-    rank_t rank() const { return price; }
 
 	int adv_id;
 	rank_t price;
@@ -34,11 +33,14 @@ struct banner
     }
 };
 
-struct banner_ex : banner
+class banner_ex : public banner
 {
-    using banner::banner;
     using rank_t = std::tuple<int, int, int, int, int>;
+    rank_t rank() const { return std::make_tuple(price, popularity, pxm_bytes, area(), perimeter()); }
 
+public:
+    friend class banner_traits<banner_ex>;
+    using banner::banner;
     banner_ex(int id, int pr, int pop)
         : banner(id, pr), popularity(pop)
     {}
@@ -56,9 +58,7 @@ struct banner_ex : banner
         return 2*(width + height);
     }
 
-    rank_t rank() const {
-        return std::make_tuple(price, popularity, pxm_bytes, area(), perimeter());
-    }
+    // ...other metrics to prioritize
 
     friend std::ostream& operator<<(std::ostream& os, const banner_ex& b) {
         return os << '{' << b.adv_id << ", " << b.price << ", " << b.popularity << "..." <<'}';
@@ -74,44 +74,6 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
 	os << ']';
 	return os;
 }
-
-template <typename BT>
-struct max_sum_price_block
-{
-    using compare_greater = typename banner_traits<BT>::greater;
-    using summator = typename banner_traits<BT>::summator;
-    using sum_t = typename banner_traits<BT>::sum_t;
-    using calc_rv_t = typename banner_traits<BT>::calc_rv;
-
-	explicit max_sum_price_block(std::size_t lots): max_sum_price_block(lots, delegate_tag{})
-	{
-	    if (m_lots < 1) throw std::logic_error("lots count should at least one");
-	}
-
-	// сортируется переданный массив: & banners
-	// offset -- индекс последнего баннера после сортировки
-    calc_rv_t operator()(std::vector<BT>& banners) noexcept {
-        // precondition: banners is not empty
-
-        // кол-во лотов (выходных баннеров)
-        const auto pivot_offset = std::min(m_lots, banners.size());
-        auto to_it = std::next(banners.begin(), pivot_offset);
-
-        // нахождение N самых дорогих баннеров + их сумма
-        std::partial_sort(banners.begin(), to_it, banners.end(), compare_greater{});
-        const auto max_bounded_price = std::accumulate(banners.begin(), to_it, sum_t{}, summator{});
-
-        return {max_bounded_price, pivot_offset, banners[0].adv_id};
-	}
-
-private:
-    // delegating cons; enabled possibility of throwing from cons
-    struct delegate_tag {};
-    max_sum_price_block(std::size_t lots, delegate_tag): m_lots(lots) {};
-
-private:
-	std::size_t m_lots;
-};
 
 template <typename T>
 class filter
@@ -142,10 +104,29 @@ template <typename BT>
 class lyciator
 {
     using calc_rv_t = typename banner_traits<BT>::calc_rv;
+    using compare_greater = typename banner_traits<BT>::greater;
+    using summator = typename banner_traits<BT>::summator;
+    using sum_t = typename banner_traits<BT>::sum_t;
     using calc_it_t = typename std::vector<calc_rv_t>::iterator;
 
     template <typename T>
     friend std::vector<T> auction(std::vector<T> banners, std::size_t lots, filter<T> banner_filter);
+
+    // сортируется переданный массив: & banners
+    // offset -- индекс последнего баннера после сортировки
+    calc_rv_t find_best_n_banners(std::size_t lots, std::vector<BT>& banners) const noexcept {
+        // precondition: banners is not empty
+
+        // кол-во лотов (выходных баннеров)
+        const auto pivot_offset = std::min(lots, banners.size());
+        auto to_it = std::next(banners.begin(), pivot_offset);
+
+        // нахождение N самых дорогих баннеров + их сумма
+        std::partial_sort(banners.begin(), to_it, banners.end(), compare_greater{});
+        const auto max_bounded_price = std::accumulate(banners.begin(), to_it, sum_t{}, summator{});
+
+        return {max_bounded_price, pivot_offset, banners[0].adv_id};
+    }
 
     explicit lyciator(std::vector<BT>&& banners)
         : rd()
@@ -165,7 +146,7 @@ class lyciator
         // разбить на группы по adv_id
         std::unordered_map<int, std::vector<BT>> by_adv;
         std::move(m_banners.begin(), end, hash_back_inserter(by_adv));
-        return std::move(by_adv);
+        return by_adv;
     }
 
     auto find_best_concurrently(std::unordered_map<int, std::vector<BT>>& by_adv_id, std::size_t lots) const
@@ -179,7 +160,9 @@ class lyciator
             // решение сути задачи
             // отправить на выделенные потоки задачи на подсчёт суммы цен для одинаковых adv_id
             for (auto&& kv : by_adv_id) {
-                max_prices.push_back(thread_pool.enqueue([&]{ return max_sum_price_block<BT>{lots}(kv.second);} ));
+                max_prices.push_back(thread_pool.enqueue([this, lots, kv]() mutable {
+                    return find_best_n_banners(lots, kv.second);
+                }));
             }
         }
 
@@ -191,8 +174,7 @@ class lyciator
                    return f.get();
                });
 
-        // TODO: NRVO or std::move (entire project question)
-        return std::move(results);
+        return results;
     }
 
     calc_it_t prioritize(std::vector<calc_rv_t>& results) const
@@ -239,8 +221,8 @@ std::vector<BT> auction(std::vector<BT> banners, std::size_t lots, filter<BT> ba
 	return lyc.run_auction(lots, std::move(banner_filter));;
 }
 
-int main() {
-
+int main()
+{
     using banner_t = banner_ex;
     std::vector<banner_t> banners {
             {1, 200, 100},
